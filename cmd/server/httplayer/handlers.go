3596cc76425ba6777152436c97b3d53e6dbd7dc2
Denis Kovtun <denis.kovtun.rus@gmail.com>
2024-03-27 14:12:48 +0300

    fixed rounding error

--- cmd/server/httplayer/handlers.go
+++ cmd/server/httplayer/handlers.go
@@ -61,7 +61,7 @@ func (api *httpAPI) getMetric(res http.ResponseWriter, req *http.Request) {
 		case gaugeMetricType:
 			memValue, isSet := gauge[memName]
 			if isSet {
-				_, err = res.Write([]byte(fmt.Sprintf("%.3f", memValue)))
+				_, err = res.Write([]byte(fmt.Sprintf("%.2f", memValue)))
 				if err != nil {
 					fmt.Printf("Server: error writing request body %s\n", err)
 				}
